4ca47ca3d098ba1f4644f04933779b528aec5a4a
CharlieWu <charliewu500@gmail.com>
2025-11-25 17:48:52 +0800

    修正 port 設定

--- src/main.ts
+++ src/main.ts
@@ -19,9 +19,12 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
-  console.log('Server running on http://localhost:3000');
-  console.log('Swagger API 文件: http://localhost:3000/api');
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+    console.log(`Swagger API 文件: http://localhost:${port}/api`);
+  });
 }
 
 bootstrap();
